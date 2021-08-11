d4a1a918dc6432d590b41de6bbc6d629c3faf4f3
mihai10001 <rvivits@gmail.com>
2021-08-11 15:29:23 +0300

    scaffold default angular application with ng new

---
+++ sorting-visualiser/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
