d9d1d54c271336dd20364d4cf042e3e1275f19be
mihai10001 <rvivits@gmail.com>
2021-08-19 11:43:56 +0300

    create shared settings service to communicate between components (with setters/getters)

---
+++ sorting-visualiser/src/app/main-layout/services/settings.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+
+  private _delay: number = 40;
+  private _inputArray: number[] = [];
+  private _inputArrayLength: number = 20;
+  private _selectedSortingFunctionKeys: Subject<string[]> = new Subject();
+  private _playSortingFunction: Subject<boolean> = new Subject();
+
+  constructor() { }
+
+  get delay(): number {
+    return this._delay;
+  }
+  set delay(delay: number) {
+    this._delay = delay;
+  }
+
+  get inputArray(): number[] {
+    return this._inputArray;
+  }
+  set inputArray(inputArray: number[]) {
+    this._inputArray = inputArray;
+  }
+
+  get inputArrayLength(): number {
+    return this._inputArrayLength;
+  }
+  set inputArrayLength(inputArrayLength: number) {
+    this._inputArrayLength = inputArrayLength;
+  }
+
+  get selectedSortingFunctionKeys(): Subject<string[]> {
+    return this._selectedSortingFunctionKeys;
+  }
+  set selectedSortingFunctionKeysValue(selectedSortingFunctionKeys: string[]) {
+    this._selectedSortingFunctionKeys.next(selectedSortingFunctionKeys);
+  }
+
+  get playSortingFunction(): Subject<boolean> {
+    return this._playSortingFunction;
+  }
+  set playSortingFunctionValue(play: boolean) {
+    this._playSortingFunction.next(play);
+  }
+}
