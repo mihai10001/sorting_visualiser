df06250f3e8792fc8ef02348827f6a95a1a2676f
mihai10001 <rvivits@gmail.com>
2021-08-27 15:00:33 +0300

    implement simple loading component

--- sorting-visualiser/src/app/main-layout/results/loading-wrapper/loading-wrapper.component.ts
+++ sorting-visualiser/src/app/main-layout/results/loading-wrapper/loading-wrapper.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+import { ResultsService } from '../../services/results.service';
 
 @Component({
   selector: 'app-loading-wrapper',
@@ -10,9 +12,13 @@ export class LoadingWrapperComponent implements OnInit {
   _isLoading: boolean | undefined = undefined;
 
   constructor(
+    private settingsService: SettingsService,
+    private resultsService: ResultsService)
   { }
 
   ngOnInit() {
+    this.settingsService.playSortingFunction.subscribe(() => this._isLoading = true);
+    this.resultsService.resultsSubject.subscribe(() => this._isLoading = (this.resultsService.results.length > 0) ? true : false);
   }
 
 }
