6aef4e0bd2a2b929523297a94a67d98f1296b2bb
mihai10001 <rvivits@gmail.com>
2021-08-30 18:00:28 +0300

    line chart implementation

--- sorting-visualiser/src/app/main-layout/results/line-chart/line-chart.component.ts
+++ sorting-visualiser/src/app/main-layout/results/line-chart/line-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataset, ChartOptions } from 'chart.js';
+import { ResultsService } from '../../services/results.service';
 
 @Component({
   selector: 'app-line-chart',
@@ -16,8 +17,23 @@ export class LineChartComponent implements OnInit {
     plugins: { legend: { labels: { color: 'white' } } }
   };
 
+  constructor(private resultsService: ResultsService) { }
 
   ngOnInit() {
+    this.resultsService.resultsSubject.subscribe(
+      () => {
+        if (this.resultsService.results.length > 0) {
+          this.lineChartData = [
+            { data: this.resultsService.results.map( result => result.totalExecutionTime ), label: 'Total execution time (ms)' },
+            { data: this.resultsService.results.map( result => result.numberOfComparisons ), label: 'Number of comparisons' },
+            { data: this.resultsService.results.map( result => result.numberOfSwaps ), label: 'Number of swaps' }
+          ];
+          this.lineChartLabels = this.resultsService.results.map( result => result.functionName );
+        } else {
+          this.lineChartData = [];
+        }
+      }
+    );
   }
 
 }
