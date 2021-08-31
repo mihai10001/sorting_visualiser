5e8253a86e8f106ea63fbd0e6c2d9fb1ef5558c9
mihai10001 <rvivits@gmail.com>
2021-08-31 17:04:58 +0300

    implement color picker on settings component for picking bar colors

--- sorting-visualiser/src/app/main-layout/settings/settings.component.ts
+++ sorting-visualiser/src/app/main-layout/settings/settings.component.ts
@@ -17,11 +17,14 @@ export class SettingsComponent implements OnInit {
     this.customizeSettingsForm = this.formBuilder.group({
       delay: [this.settingsService.delay],
       inputArrayLength: [this.settingsService.inputArrayLength],
+      barColor: [this.settingsService.colors.barColor],
+      highlightedBarColor: [this.settingsService.colors.highlightedBarColor],
     });
 
     this.customizeSettingsForm.valueChanges.subscribe(formValue => {
       this.settingsService.delay = formValue.delay;
       this.settingsService.inputArrayLength = formValue.inputArrayLength;
+      this.settingsService.colors = { 'barColor': formValue.barColor, 'highlightedBarColor': formValue.highlightedBarColor } as ColorType;
     });
   }
 }
